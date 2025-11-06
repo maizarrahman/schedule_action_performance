59ab5017ca1a52a38688d214dc12ab218d8cb242
Maizar Rahman <44960902+maizarrahman@users.noreply.github.com>
2025-11-06 07:46:01 +0700

    Add __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Schedule Action Performance",
+    "summary": "Track execution time and succesful of each ir.cron job",
+    "version": "15.0.1.0.0",
+    "author": "Maizar + ChatGPT",
+    "category": "Technical",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+}
